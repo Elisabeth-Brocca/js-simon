e256425f928c351d6a00f24249aea75acfc38108
ElisabethBrocca <elisabeth.brocca.07@gmail.com>
2025-10-16 16:45:34 +0200

    [UPD] base variabili + rng

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 //Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
 
+const numbers = [];
+const answers = [];
+const message = document.getElementById('message');
+const button = document.querySelector('button');
+const countdown = document.getElementById('countdown');
+const numbersList = document.getElementById('number-list');
+const answersForm = document.getElementById('answers-form');
+
+function RNG(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 //Dopo 30 secondi i numeri scompaiono e appaiono invece 5 input in cui l'utente deve inserire i numeri che ha visto precedentemente, nell'ordine che preferisce.
 
 //Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
